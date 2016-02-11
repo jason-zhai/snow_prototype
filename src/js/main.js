194bcbf16618db02c258a527c9ea93ac86204bf4
Jason Zhai <jzhai@cliqr.com>
2016-02-10 16:25:19 -0800

    basic add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,24 @@
 $('select.dropdown')
   .dropdown()
 ;
+
+
+(function() {
+    var defaultMapping = $('#tab1').clone();
+
+    $('[data-toggle="add"]').click(function() {
+        var tabIndex = $(this).index();
+        var tabOthers = $(this).siblings();
+        var tabNew = $(this).prev().clone();
+        var tabMenu = $(this).closest('.ui.menu');
+
+        tabNew.addClass('active');
+        tabNew.attr('href', '#tab' + (tabIndex+1));
+        defaultMapping.attr('id', 'tab' + (tabIndex+1));
+
+        tabOthers.removeClass('active');
+        tabNew.insertBefore($(this));
+        $("[id^='tab']").removeClass('active');
+        $('.ui.text.container').append(defaultMapping.clone());
+    });
+}());
