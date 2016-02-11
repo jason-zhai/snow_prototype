620e58033d14705786f77ef0b5d2e5bf4c45a263
Jason Zhai <jzhai@cliqr.com>
2016-02-10 18:05:14 -0800

    fix cloning conflicts with events

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,50 @@
 
-$('select.dropdown')
-  .dropdown()
-;
+
 
 
 (function() {
     var defaultMapping = $('#tab1').clone();
+    $('select.dropdown')
+  .dropdown()
+;
     var tabNumber = $('[data-toggle="tab"]').length;
+    var tabRemove = function(e) {
+        var tabItem = $(this).parent();
+        e.stopPropagation();
+        if(tabItem.hasClass('active')) {
+            if((tabItem.index() + 1) === $('[data-toggle="tab"]').length) {
+                tabItem.prev().addClass('active');
+                $(tabItem.attr('href')).prev().addClass('active');
+            } else {
+
+                tabItem.next().addClass('active');
+                $(tabItem.attr('href')).next().addClass('active');
+            }
+        }
+            tabItem.remove();
+            $(tabItem.attr('href')).remove();
+
+    };
 
     $('[data-toggle="add"]').click(function() {
         tabNumber += 1;
         var tabOthers = $(this).siblings();
         var tabNew = $(this).prev().clone();
         var tabMenu = $(this).closest('.ui.menu');
+        var tabContent = defaultMapping.clone();
 
         tabNew.addClass('active');
         tabNew.attr('href', '#tab' + tabNumber);
-        defaultMapping.attr('id', 'tab' + tabNumber);
+        tabNew.find('i.remove').click(tabRemove);
 
         tabOthers.removeClass('active');
         tabNew.insertBefore($(this));
         $("[id^='tab']").removeClass('active');
-        $('.ui.text.container').append(defaultMapping.clone());
+
+        tabContent.attr('id', 'tab' + tabNumber);
+        tabContent.find('select.dropdown').dropdown();
+        $('.ui.text.container').append(tabContent);
     });
+
+    $('[data-toggle="tab"] > i.remove').click(tabRemove);
 }());
