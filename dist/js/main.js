2a4a6d74f317e73831dbc9a8170832085f3f460a
Jason Zhai <jzhai@cliqr.com>
2016-02-11 14:03:47 -0800

    one tab left no remove

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,10 +4,9 @@
 
 (function() {
     var defaultMapping = $('#tab1').clone();
-    $('select.dropdown')
-  .dropdown()
-;
-    var tabNumber = $('[data-toggle="tab"]').length;
+    $('select.dropdown').dropdown();
+
+    var tabInitNumber = $('[data-toggle="tab"]').length;
     var tabRemove = function(e) {
         var tabItem = $(this).parent();
         e.stopPropagation();
@@ -21,29 +20,40 @@
                 $(tabItem.attr('href')).next().addClass('active');
             }
         }
-            tabItem.remove();
-            $(tabItem.attr('href')).remove();
-
+        tabItem.remove();
+        $(tabItem.attr('href')).remove();
+        tabOneLeft();
+    };
+    var tabOneLeft = function() {
+        if($('[data-toggle="tab"]').length === 1) {
+            $('i.remove').hide();
+        } else {
+            $('i.remove').show();
+        }
     };
 
+    tabOneLeft();
+
     $('[data-toggle="add"]').click(function() {
-        tabNumber += 1;
+        tabInitNumber += 1;
         var tabOthers = $(this).siblings();
         var tabNew = $(this).prev().clone();
         var tabMenu = $(this).closest('.ui.menu');
         var tabContent = defaultMapping.clone();
 
         tabNew.addClass('active');
-        tabNew.attr('href', '#tab' + tabNumber);
+        tabNew.attr('href', '#tab' + tabInitNumber);
         tabNew.find('i.remove').click(tabRemove);
 
         tabOthers.removeClass('active');
         tabNew.insertBefore($(this));
         $("[id^='tab']").removeClass('active');
 
-        tabContent.attr('id', 'tab' + tabNumber);
+        tabContent.attr('id', 'tab' + tabInitNumber);
         tabContent.find('select.dropdown').dropdown();
         $('.ui.text.container').append(tabContent);
+
+        tabOneLeft();
     });
 
     $('[data-toggle="tab"] > i.remove').click(tabRemove);
