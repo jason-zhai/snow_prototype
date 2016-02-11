76780a4aa2880a7f2a10b2726f62b6f8b10e8ace
Jason Zhai <jzhai@cliqr.com>
2016-02-10 16:33:33 -0800

    improve tab number calc

--- src/js/main.js
+++ src/js/main.js
@@ -6,16 +6,17 @@ $('select.dropdown')
 
 (function() {
     var defaultMapping = $('#tab1').clone();
+    var tabNumber = $('[data-toggle="tab"]').length;
 
     $('[data-toggle="add"]').click(function() {
-        var tabIndex = $(this).index();
+        tabNumber += 1;
         var tabOthers = $(this).siblings();
         var tabNew = $(this).prev().clone();
         var tabMenu = $(this).closest('.ui.menu');
 
         tabNew.addClass('active');
-        tabNew.attr('href', '#tab' + (tabIndex+1));
-        defaultMapping.attr('id', 'tab' + (tabIndex+1));
+        tabNew.attr('href', '#tab' + tabNumber);
+        defaultMapping.attr('id', 'tab' + tabNumber);
 
         tabOthers.removeClass('active');
         tabNew.insertBefore($(this));
